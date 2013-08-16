9606bf744c234edeef6def3fef35a8bff4726f8c
mohanraj23 <smmohanraj23@gmail.com>
2013-08-16 22:17:31 +0530

    Initialize rep

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pencilneck::Application.config.secret_token = '19b9b67aefe911595aec5dd402775d8ecd1c93924a9295c0a428300bf8a4f7911a7aa9c53782682102982dc96a09b458cea5da591d6dd3e9ca8b46c0f1329560'
